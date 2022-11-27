ed01a32af2008e3b7ff0981c756ccd5afbbf3e97
jonpaco <jon.cardinale@gmail.com>
2022-11-27 15:07:32 -0500

    Install the square wave resource. Figure out sound structure later.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@
 extern crate lazy_static;
 
 use bevy::prelude::*;
+use bevy_kira_audio::prelude::*;
 use bevy_egui::EguiPlugin;
 mod display;
 mod keyboard;
+mod speaker;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
+        .add_plugin(AudioPlugin)
         .add_startup_system(display::install_display)
         .add_startup_system(keyboard::install_keyboard)
+        .add_startup_system(speaker::install_speaker)
         .add_system(display::render)
         .add_system(display::test_render)
         .add_system(keyboard::handle_keyboard)
--- src/speaker.rs
+++ src/speaker.rs
@@ -1,8 +1,27 @@
+use std::collections::HashMap;
 use bevy::prelude::*;
+use bevy_kira_audio::prelude::*;
 
-#[derive(Component)]
-struct Speaker {
-
+lazy_static! {
+static ref SOUND_POOL: HashMap<&'static str, &'static str> = {
+        HashMap::from([
+            ("primary", "square440.wav"),
+            ])
+    };
 }
 
+#[derive(Resource)]
+struct Square {
+    source: Handle<AudioSource>,
+    gain: u8,
+}
 
+pub fn install_speaker(mut commands: Commands, server: Res<AssetServer>) {
+    if let Some(primary) = SOUND_POOL.get("primary") {
+        let audio: Handle<AudioSource> = server.load(*primary);
+        commands.insert_resource(Square {
+            source: audio,
+            gain: 1,
+        });
+    }
+}
