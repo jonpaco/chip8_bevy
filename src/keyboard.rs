2fbb0e22b6926d3e2e729bc023353c4f1c725bf3
jonpaco <jon.cardinale@gmail.com>
2022-11-26 20:09:01 -0500

    get keyboard working, start audio

---
+++ src/keyboard.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+use bevy::prelude::*;
+use bevy::input::ButtonState;
+use bevy::input::keyboard::KeyboardInput;
+
+lazy_static! {
+static ref KEYMAP: HashMap<KeyCode, u8> = {
+        HashMap::from([
+            (KeyCode::Key1, 0x1),
+            (KeyCode::Key2, 0x2),
+            (KeyCode::Key3, 0x3),
+            (KeyCode::Key4, 0xc),
+            (KeyCode::Q, 0x4),
+            (KeyCode::W, 0x5),
+            (KeyCode::E, 0x6),
+            (KeyCode::R, 0xD),
+            (KeyCode::A, 0x7),
+            (KeyCode::S, 0x8),
+            (KeyCode::D, 0x9),
+            (KeyCode::F, 0xE),
+            (KeyCode::Z, 0xA),
+            (KeyCode::X, 0x0),
+            (KeyCode::C, 0xB),
+            (KeyCode::V, 0xF),
+        ])
+    };
+}
+
+#[derive(Component)]
+pub struct Keyboard {
+    keylist: Vec<ButtonState>,
+}
+
+impl Keyboard {
+    fn new() -> Self {
+        Self {
+            keylist: vec![ButtonState::Released; KEYMAP.keys().len()],
+        }
+    }
+}
+
+pub fn install_keyboard(mut commands: Commands) {
+    commands.spawn(Keyboard::new());
+}
+
+pub fn handle_keyboard(mut key_evr: EventReader<KeyboardInput>, mut query: Query<&mut Keyboard>) {
+    for ev in key_evr.iter() {
+        if let Some(keycode) = ev.key_code {
+        let mut board = query.single_mut();
+            if let Some(index) = KEYMAP.get(&keycode) {
+                board.keylist[*index as usize] = ev.state;
+            }
+        }
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+#[macro_use]
+extern crate lazy_static;
+
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 mod display;
+mod keyboard;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_startup_system(display::install_display)
+        .add_startup_system(keyboard::install_keyboard)
         .add_system(display::render)
         .add_system(display::test_render)
+        .add_system(keyboard::handle_keyboard)
         .run();
 }
 
