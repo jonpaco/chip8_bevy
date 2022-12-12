d44bd59d3c89c8f803419e0d8369387bc785b5cb
jonpaco <jon.cardinale@gmail.com>
2022-12-11 19:04:29 -0500

    Update main window plugin settings

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 extern crate rand;
 
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 use bevy_kira_audio::prelude::*;
 use bevy::time::FixedTimestep;
 use bevy_egui::EguiPlugin;
@@ -14,7 +15,15 @@ mod cpu;
 fn main() {
     App::new()
         .add_event::<display::InstallProgram>()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set( WindowPlugin {
+                window: WindowDescriptor {
+                title: "Chip8".to_string(),
+                present_mode: PresentMode::Immediate,
+                resizable: true,
+                ..default()
+                },
+                ..default()
+        }))
         .add_plugin(EguiPlugin)
         .add_plugin(AudioPlugin)
         .add_startup_system(display::install_display)
@@ -26,7 +35,7 @@ fn main() {
         .add_system(cpu::cpu_event_handler)
         .add_system_set(
             SystemSet::new()
-            .with_run_criteria(FixedTimestep::step(1.0/6000.0))
+            .with_run_criteria(FixedTimestep::step(1.0/60.0))
             .with_system(cpu::update_cpu)
         )
         .run();
